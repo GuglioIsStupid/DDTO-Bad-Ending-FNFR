09c1f263b6b83d81bdd3a930fefc0aa0909dd8aa
GuglioIsStupid <bradsbluetab@gmail.com>
2022-09-27 15:36:20 -0400

    balls

--- src/love/input.lua
+++ src/love/input.lua
@@ -35,7 +35,7 @@ if love.system.getOS() == "NX" then
 			gameUp = {"axis:lefty-", "axis:righty-", "button:rightshoulder", "button:dpup", "button:y", "key:" .. customBindUp, "key:up"},
 			gameRight = {"axis:triggerright+", "axis:leftx+", "axis:rightx+", "button:dpright", "button:b", "key:" .. customBindRight, "key:right"},
 			gameBack = {"button:start", "key:escape"},
-			BALLS = {"key:+"}
+			BALLS = {"key:9"}
 		},
 		joystick = love.joystick.getJoysticks()[1]
 	}
@@ -57,6 +57,7 @@ else
 			gameUp = {"key:" .. customBindUp, "key:up", "axis:lefty-", "axis:righty-", "button:rightshoulder", "button:dpup", "button:y"},
 			gameRight = {"key:" .. customBindRight, "key:right", "axis:triggerright+", "axis:leftx+", "axis:rightx+", "button:dpright", "button:b"},
 			gameBack = {"key:escape", "button:start"},
+			BALLS = {"key:9"}
 		},
 		joystick = love.joystick.getJoysticks()[1]
 	}
